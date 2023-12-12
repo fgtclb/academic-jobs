b103774af376ea958d5946ee6f3b4d6e91f94db3
Riad <riad@web-vision.de>
2023-12-12 13:05:19 +0100

    [TASK] Release 0.1.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '0.1.5',
+    'version' => '0.1.6',
     'clearCacheOnLoad' => true,
     'category' => 'fe,be',
     'author' => 'Riad Zejnilagic Trumic',
