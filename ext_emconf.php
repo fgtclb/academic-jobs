5a1a549ef89e74f3e12680e3d07738eba2be8630
Stefan Bürk <stefan@buerk.tech>
2025-03-20 06:48:06 +0100

    [TASK] Align `ext_emconf.php` sysext versions
    After branching `1.2.x` away the main branch
    has been updated to be the development branch
    for `2.x.x` along with quite some changes.
    
    However, during all these excessive commands
    used to archive the outcome over nightshift,
    it has been missed to adjust the TYPO3 version
    constraints in the `ext_emconf.php` files.
    
    Do it now!

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'The Academic Jobs extension allows users to create and manage job postings.',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
     ],
     'state' => 'beta',
