16251ff1c2018abbbce4b055e66836188e13eb57
Stefan Bürk <stefan@buerk.tech>
2025-05-06 21:21:52 +0200

    [TASK] Set version to `2.0.2-dev`

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'clearCacheOnLoad' => true,
     'category' => 'fe,be',
     'author' => 'Riad Zejnilagic Trumic',
