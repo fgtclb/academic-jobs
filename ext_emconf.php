12e1015cc860555ff8231d5766f982179c5d57c4
Stefan Bürk <stefan@buerk.tech>
2025-07-25 11:31:44 +0200

    [RELEASE] 1.2.0
    Used command(s):
    
    ```shell
    clear ; USE_VERSION='1.2.0' ; \
    for packageFolder in packages/fgtclb/*; do \
        composerPackageName="$( jq -r '.name' ${packageFolder}/composer.json )" ; \
        extensionKey="$( jq -r '.extra."typo3/cms"."extension-key"' ${packageFolder}/composer.json )" ; \
        packageNames+=("${composerPackageName}") ; \
        echo "${USE_VERSION}" > ${packageFolder}/VERSION ; \
        tailor set-version ${USE_VERSION} --path ${packageFolder} ; \
    done
    ```

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '1.1.5',
+    'version' => '1.2.0',
     'clearCacheOnLoad' => true,
     'category' => 'fe,be',
     'author' => 'Riad Zejnilagic Trumic',
