1fc6c71cdf481ca8b460436dad4c14ff5a2f447b
Sergio Catalá <s.catala@web-vision.de>
2025-08-18 15:39:46 +0200

    [TASK] Inject Logger via constructor and remove LoggerAwareTrait
    This commit refactors `ImageUploadConverter` to inject the compatible
    `LoggerInterface` via the constructor instead of using the
    `LoggerAwareTrait`.
    
    Instantiation of the logger with dependency injection is now the
    recommended procedure. The `LoggerAwareTrait` and
    `LoggerAwareInterface` are discouraged in favor of direct dependency
    injection[1].
    
    Additionally, the nullsafe operator (?->) has been removed from the
    logger calls, as the logger is now guaranteed to be available after
    being injected via the constructor.
    
    [1] https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/11.4/Feature-95044-SupportAutowiredLoggerInterfaceInjection.html

--- Classes/Property/TypeConverter/ImageUploadConverter.php
+++ Classes/Property/TypeConverter/ImageUploadConverter.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace FGTCLB\AcademicJobs\Property\TypeConverter;
 
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
 use TYPO3\CMS\Core\Http\UploadedFile;
 use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
 use TYPO3\CMS\Core\Resource\DuplicationBehavior;
@@ -25,10 +24,8 @@ use TYPO3\CMS\Extbase\Property\PropertyMappingConfigurationInterface;
 use TYPO3\CMS\Extbase\Property\TypeConverter\AbstractTypeConverter;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
-final class ImageUploadConverter extends AbstractTypeConverter implements LoggerAwareInterface
+final class ImageUploadConverter extends AbstractTypeConverter
 {
-    use LoggerAwareTrait;
-
     public const CONFIGURATION_TARGET_DIRECTORY_COMBINED_IDENTIFIER = 'targetFolderCombinedIdentifier';
     public const CONFIGURATION_MAX_UPLOAD_SIZE = 'maxUploadSize';
     public const CONFIGURATION_ALLOWED_MIME_TYPES = 'allowedMimeTypes';
@@ -40,8 +37,9 @@ final class ImageUploadConverter extends AbstractTypeConverter implements Logger
     protected $priority = 10;
 
     public function __construct(
-        protected ResourceFactory $resourceFactory,
-        protected LanguageServiceFactory $languageServiceFactory
+        private readonly ResourceFactory $resourceFactory,
+        private readonly LanguageServiceFactory $languageServiceFactory,
+        private readonly LoggerInterface $logger,
     ) {}
 
     /**
@@ -229,28 +227,28 @@ final class ImageUploadConverter extends AbstractTypeConverter implements Logger
 
         switch ($errorCode) {
             case \UPLOAD_ERR_INI_SIZE:
-                $this->logger?->error('The uploaded file exceeds the upload_max_filesize directive in php.ini.');
+                $this->logger->error('The uploaded file exceeds the upload_max_filesize directive in php.ini.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530345');
             case \UPLOAD_ERR_FORM_SIZE:
-                $this->logger?->error('The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.');
+                $this->logger->error('The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530345');
             case \UPLOAD_ERR_PARTIAL:
-                $this->logger?->error('The uploaded file was only partially uploaded.');
+                $this->logger->error('The uploaded file was only partially uploaded.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530346');
             case \UPLOAD_ERR_NO_FILE:
-                $this->logger?->error('No file was uploaded.');
+                $this->logger->error('No file was uploaded.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530347');
             case \UPLOAD_ERR_NO_TMP_DIR:
-                $this->logger?->error('Missing a temporary folder.');
+                $this->logger->error('Missing a temporary folder.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530348');
             case \UPLOAD_ERR_CANT_WRITE:
-                $this->logger?->error('Failed to write file to disk.');
+                $this->logger->error('Failed to write file to disk.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530348');
             case \UPLOAD_ERR_EXTENSION:
-                $this->logger?->error('File upload stopped by extension.');
+                $this->logger->error('File upload stopped by extension.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530348');
             default:
-                $this->logger?->error('Unknown upload error.');
+                $this->logger->error('Unknown upload error.');
                 return $typoScriptFrontendController->sL('LLL:EXT:academic_jobs/Resources/Private/Language/locallang.xlf:upload.error.150530348');
         }
     }
