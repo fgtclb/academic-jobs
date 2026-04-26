f4ecbff493c84f8754723a20d6913421f5c11de9
Stefan Bürk <stefan@buerk.tech>
2026-04-26 16:52:16 +0200

    [TASK] DPL-81: Streamline unit- and functional basic tests
    Each provided extension should have at least following
    basic tests:
    
    * Verify against supported TYPO3 core versions
      as unit and functional test.
    
    * Verify loaded extensions in tests it depends
      on as functional test.
    
    To make this maintanable in a simple way two generic
    traits are added:
    
    * `\FGTCLB\TestingHelper\FunctionalTestCase\ExtensionCoreVersionCompatTestsTrait`
    * `\FGTCLB\TestingHelper\FunctionalTestCase\ExtensionsLoadedTestsTrait`
    
    All `pacakges/*/*` are streamlined to provide following
    tests:
    
    * `Tests/Unit/VersionCompatTest.php`
    * `Tests/Functional/VersionCompatTest.php`
    * `Tests/Functional/ExtensionsLoadedTest.php`
    
    Functional tests uses the corresponding abstract testcase
    provided by that extension to share the default loaded
    extensions in the functional test instance.

--- Tests/Functional/ExtensionLoadedTest.php
+++ Tests/Functional/ExtensionLoadedTest.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 
 namespace FGTCLB\AcademicJobs\Tests\Functional;
 
-use PHPUnit\Framework\Attributes\Test;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use FGTCLB\TestingHelper\FunctionalTestCase\ExtensionsLoadedTestsTrait;
 
 final class ExtensionLoadedTest extends AbstractAcademicJobsTestCase
 {
-    #[Test]
-    public function testCaseLoadsExtension(): void
-    {
-        $this->assertContains('fgtclb/academic-jobs', $this->testExtensionsToLoad);
-    }
+    use ExtensionsLoadedTestsTrait;
 
-    #[Test]
-    public function extensionIsLoaded(): void
-    {
-        $this->assertTrue(ExtensionManagementUtility::isLoaded('academic_jobs'));
-    }
+    private static $expectedLoadedExtensions = [
+        // composer package names
+        'fgtclb/academic-base',
+        'fgtclb/academic-jobs',
+        // extension keys
+        'academic_base',
+        'academic_jobs',
+    ];
 }
